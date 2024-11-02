675a44f14655e5bbf17f8aedf5257534b761bf41
petapp80 <pet.adoption.project.mg@gmail.com>
2024-11-02 15:39:01 +0530

    login screen uodated

---
+++ Adoption/pet app/lib/user/screens/login screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyLoginScreen extends StatelessWidget {
+  const MyLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar:AppBar(backgroundColor: Colors.blue));
+  }
+}
